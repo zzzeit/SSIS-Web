59eb1912f0cecfc83ee10ca91c9bb8ea5c9a75f0
Nel <neilanthony.balbutin@g.msuiit.edu.ph>
2025-09-10 20:21:59 +0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# 2. Create an instance of the Flask class
+app = Flask(__name__)
+
+# 3. Define a route and a function to run when the route is accessed
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+# This part is optional but good practice to run the app
+if __name__ == "__main__":
+    app.run(debug=True)
