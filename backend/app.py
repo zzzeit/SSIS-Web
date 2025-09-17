414b259fdb3ba83d6706077fcf3411ad1cc74931
Nel <79737255+zzzeit@users.noreply.github.com>
2025-09-17 08:11:00 +0800

    Imported files from SSIS-V2

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+@app.route("/test")
+def test():
+    return "Test"
+
 # This part is optional but good practice to run the app
 if __name__ == "__main__":
     app.run(debug=True)
