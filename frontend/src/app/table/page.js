f81f0d587c5fb930b5ff95e000693ac77b3e1626
Nel <79737255+zzzeit@users.noreply.github.com>
2025-10-01 23:05:53 +0800

    updated table component

--- frontend/src/app/table/page.js
+++ frontend/src/app/table/page.js
@@ -1,23 +1,30 @@
 import './table.css'
 
-export default async function Table() {
+export default async function Table({ table_name="Table", headers=["header1", "header2", "header3"] }) {
     // const data = await fetch('http://127.0.0.1:5000/get/students')
     // const res = await data.json()
 
 
     return (
     <>
+        <div className='table-header'>
+            <label>{table_name}</label>
+        </div>
         <div className='my-table'>
             <table> 
                 <thead>
                     <tr>
-                        <th>First Name</th>
+                        {/* <th>First Name</th>
                         <th>Last Name</th>
                         <th>Sex</th>
                         <th>ID Number</th>
                         <th>Year Level</th>
                         <th>College</th>
-                        <th>Program</th>
+                        <th>Program</th> */}
+
+                        {headers.map((header) => (
+                            <th key={header}>{header}</th>
+                        ))}
                     </tr>
                 </thead>
 
